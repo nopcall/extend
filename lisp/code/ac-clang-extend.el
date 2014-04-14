86a9a5612ce26c4ff2c4de440ef16aef0781d087
nopcall <nopcall@gmail.com>
2014-04-14 16:02:55 +0800

    stolen config from oh-my-emacs

--- lisp/code/ac-clang-extend.el
+++ lisp/code/ac-clang-extend.el
@@ -21,28 +21,50 @@
   (progn
     (require-package 'auto-complete-clang)
     (require 'auto-complete-clang)
-    (defun my-ac-cc-mode-setup ()
-      (setq ac-sources (append '(ac-source-clang ac-source-yasnippet) ac-sources)))
-    (add-hook 'c-mode-common-hook 'my-ac-cc-mode-setup)				;; ac-source-gtags
-
-    (defun my-ac-config ()
-      (setq-default ac-sources '(ac-source-abbrev ac-source-dictionary ac-source-words-in-same-mode-buffers))
-      (add-hook 'emacs-lisp-mode-hook 'ac-emacs-lisp-mode-setup)
-      ;; (add-hook 'c-mode-common-hook 'ac-cc-mode-setup)
-      (add-hook 'ruby-mode-hook 'ac-ruby-mode-setup)
-      (add-hook 'css-mode-hook 'ac-css-mode-setup)
-      (add-hook 'auto-complete-mode-hook 'ac-common-setup)
-      (global-auto-complete-mode t))
+    ;; (setq command "echo | g++ -v -x c++ -E - 2>&1 |
+    ;;              grep -A 20 starts | grep include | grep -v search")
+    (setq command "/usr/include/c++/4.8
+		/usr/include/x86_64-linux-gnu/c++/4.8
+		/usr/include/c++/4.8/backward
+		/usr/lib/gcc/x86_64-linux-gnu/4.8/include
+		/usr/local/include
+		/usr/lib/gcc/x86_64-linux-gnu/4.8/include-fixed
+		/usr/include/x86_64-linux-gnu
+		/usr/include")
+    (setq ac-clang-flags
+	  (mapcar (lambda (item)
+		    (concat "-I" item))
+		  (split-string
+		   (shell-command-to-string command))))
+    ;; completion for C/C++ macros.
+    (push "-code-completion-macros" ac-clang-flags)
+    (push "-code-completion-patterns" ac-clang-flags)
+    (dolist (mode-hook '(c-mode-hook c++-mode-hook))
+      (add-hook mode-hook
+		(lambda ()
+		  (add-to-list 'ac-sources 'ac-source-clang))))
     (message "Using *auto-complete-clang* for you C/C++."))			;; clang
   )
 
-(when (file-exists-p "/usr/include/qt4")
-  (setq ac-clang-flags
-	(mapcar (lambda (f) (concat "-I" f))
-		(directory-files "/usr/include/qt4" t "Qt\\w+")))
-  (setq ac-clang-cflags ac-clang-flags))					;; Include Qt4 if installed
+(defun ome-pkg-config-enable-clang-flag (pkg-config-lib)
+  "This function will add necessary header file path of a
+specified by `pkg-config-lib' to `ac-clang-flags', which make it
+completionable by auto-complete-clang"
+  (interactive "spkg-config lib: ")
+  (if (executable-find "pkg-config")
+      (if (= (shell-command
+              (format "pkg-config %s" pkg-config-lib))
+             0)
+          (setq ac-clang-flags
+                (append ac-clang-flags
+                        (split-string
+                         (shell-command-to-string
+                          (format "pkg-config --cflags-only-I %s"
+                                  pkg-config-lib)))))
+        (message "Error, pkg-config lib %s not found." pkg-config-lib))
+    (message "Error: pkg-config tool not found.")))
 
-(my-ac-config)
+;;(ome-pkg-config-enable-clang-flag "QtGui")
 
 (provide 'ac-clang-extend)
 ;;; clang-async-extend.el ends here
