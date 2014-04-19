cfaf24f62d209b295d0bb85d139756f51a062fdb
Nopcall <nopcall[at]gmail.com>
2014-04-19 12:00:06 +0800

    lazy lazy.

--- lisp/custom-extend.el
+++ lisp/custom-extend.el
@@ -13,7 +13,7 @@
  '(apropos-do-all t)
  '(auto-image-file-mode t)							;; display image
  '(echo-keystrokes 0.1)								;; show pressed key after 0.1 second
- '(save-interprogram-paste-before-kill nil)					;; set for iClipboard in Mac
+ ;; '(save-interprogram-paste-before-kill nil)					;; set for iClipboard in Mac
  '(gc-cons-threshold 20000000)							;; Garbage collect optimization
  '(garbage-collection-messages t)
  ;; Backup setting -------------------------------------------------------------
@@ -55,7 +55,7 @@
  '(blink-cursor-mode nil)							;; do not cursor blink
  '(require-final-newline nil)							;; 不自动添加换行符到末尾, 有些情况会出现错误
  '(fill-column 80)								;; 设置80行自动换行 只在text-mode有效果
- '(mouse-avoidance-mode (quote exile) nil (avoid))				;; 鼠标躲避光标 animate
+ '(mouse-avoidance-mode (quote animate) nil (avoid))				;; 鼠标躲避光标 animate exile
  ;; '(resize-mini-windows nil t)						;; this make some error whit ido-vertical-mode
  ;; '(sentence-end "\\([。！？]\\|……\\|[.?!][]\"')}]*\\($\\|[ ]\\)\\)[]*")	;; 会引起help模式下字符的问题
  '(session-use-package t nil (session))
