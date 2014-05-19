d609845b7693285ee375bacb5d0d925349e0b2a3
Nopcall <nopcall[at]gmail.com>
2014-05-20 01:45:43 +0800

    another implementation to add third library

--- lisp/code/irony-extend.el
+++ lisp/code/irony-extend.el
@@ -10,31 +10,50 @@
 (irony-enable 'ac)								;; the ac plugin will be activated in each buffer using irony-mode
 (setq ac-disable-faces (delq 'font-lock-string-face ac-disable-faces))
 
-;; (setq irony-cdb-cmake-generator "Ninja")					;; Use Ninja (http://martine.github.io/ninja/) instead of classic Makefiles
+(setq irony-cdb-cmake-generator "Ninja")					;; Use Ninja (http://martine.github.io/ninja/) instead of classic Makefiles
 
-(setq qt-include-path-prefix "/home/Data/Qt5.2.1/5.2.1/gcc_64/include/")	;; Qt5 library
-(when (file-exists-p qt-include-path-prefix)
-  (setq	irony-libclang-additional-flags
-	(mapcar (lambda (include-dirs) (concat "-I" include-dirs))
-		(directory-files qt-include-path-prefix t "\\w+"))))
-(setq irony-libclang-additional-flags
-      (append '("-I/home/Data/Qt5.2.1/5.2.1/gcc_64/include"
-		"-code-completion-macros"					;; completion for C/C++ macros.
-		"-code-completion-patterns"
-		"-std=c++11"
-		)
-	      irony-libclang-additional-flags))
+(defun qt5-irony ()
+  "Enable Qt library for buffer then make it local"
+  (interactive)
+  (when (not (boundp 'qt-include-path-prefix))
+    (setq qt-include-path-prefix "/home/Data/Qt5.2.1/5.2.1/gcc_64/include"))	;; set Qt library path unless it's been defined
+  (when (file-exists-p qt-include-path-prefix)
+    (setq irony-libclang-additional-flags
+	  (append
+	   (mapcar (lambda (include-dirs) (concat "-I" include-dirs))
+		   (directory-files qt-include-path-prefix t "\\w+"))
+	   (split-string (concat "-I" qt-include-path-prefix))
+	   irony-libclang-additional-flags)))
+  ;; (push "-code-completion-macros" irony-libclang-additional-flags)
+  ;; (push "-code-completion-patterns" irony-libclang-additional-flags)
+  (make-variable-buffer-local 'irony-libclang-additional-flags)
+  (irony-reload-flags))
 
-(defun irony-c++-hooks ()
-  "Enable the hooks in the preferred order: 'yas -> auto-complete -> irony'."
-  ;; if yas is not set before (auto-complete-mode 1), overlays may persist after
-  ;; an expansion.
-  (when (member major-mode irony-known-modes)
-    (setq ac-sources nil)							;; uncomment if other ac-sources are too annoying
-    (irony-mode t)))								;; enable irony-mode
+(defun pkg-lib-include-extend (pkg-config-lib)
+  "Add necessary header file path for code completion.
+append `PKG-CONFIG-LIB' to `irony-libclang-additional-flags'"
+  (interactive "spkg-config lib: ")
+  (if (executable-find "pkg-config")
+      (if (= (shell-command (format "pkg-config %s" pkg-config-lib))
+	     0)
+          (progn (setq irony-libclang-additional-flags
+		       (append irony-libclang-additional-flags
+			       (split-string
+				(shell-command-to-string
+				 (format "pkg-config --cflags-only-I %s"
+					 pkg-config-lib)))))
+		 (irony-reload-flags))
+        (message "Error, pkg-config lib %s not found." pkg-config-lib))
+    (message "Error: pkg-config tool not found.")))
 
-(add-hook 'c++-mode-hook 'irony-c++-hooks)
-(add-hook 'c-mode-hook 'irony-c++-hooks)
+(defun enable-irony-mode-extend ()
+  (when (member major-mode irony-known-modes)					;; avoid enabling irony-mode in modes that inherits c-mode, e.g: php-mode
+    ;; (setq ac-sources nil)							;; uncomment if other ac-sources are too annoying
+    (irony-mode t)
+    (define-key irony-mode-map (kbd "<s-tab>") 'ac-complete-irony)))		;; enable irony-mode
+
+(add-hook 'c++-mode-hook 'enable-irony-mode-extend)
+(add-hook 'c-mode-hook 'enable-irony-mode-extend)
 
 (message "Using *auto-complete-clang* for you C/C++.")
 
