93548cafbdf081fef2d4f3c10506a3b1065be5d5
Nopcall <nopcall[at]gmail.com>
2014-05-20 01:46:01 +0800

    another implementation to add third library

--- lisp/code/flycheck-extend.el
+++ lisp/code/flycheck-extend.el
@@ -1,28 +1,17 @@
-;;;  delete
-;; (eval-after-load 'flycheck '(setq flycheck-checkers (delq 'emacs-lisp-checkdoc flycheck-checkers)))
+;;; delete lisp warning
+(eval-after-load 'flycheck '(setq flycheck-checkers (delq 'emacs-lisp-checkdoc flycheck-checkers)))
 
-;; std
+;; c++11
 (add-hook 'c++-mode-hook (lambda() (setq flycheck-clang-language-standard "c++11")))
 
 ;;; Qt include path
-(setq flycheck-qt-include-prefix "/home/Data/Qt5.2.1/5.2.1/gcc_64/include")
-(when (file-exists-p flycheck-qt-include-prefix)
-  (setq	flycheck-clang-include-path
-	(directory-files flycheck-qt-include-prefix t "\\w+")))
-
-(setq flycheck-clang-include-path
-      (append '("/home/Data/Qt5.2.1/5.2.1/gcc_64/include")
-	      flycheck-clang-include-path))
-
-(defun flycheck-clang-qt-path ()
-  (interactive)
-  (let ((dirs (directory-files flycheck-qt-include-prefix)))
-    (dolist (var dirs)
-      ;; (print var)
-      (unless (string-match "^\\." var)
-        (add-to-list 'flycheck-clang-include-path
-		     (expand-file-name var flycheck-qt-include-prefix))))))
-
-;; (flycheck-clang-qt-path)
+(when (not (boundp 'qt-include-path-prefix))
+  (setq qt-include-path-prefix "/home/Data/Qt5.2.1/5.2.1/gcc_64/include"))	;; set Qt library path unless it's been defined
+(when (file-exists-p qt-include-path-prefix)
+  (setq flycheck-clang-include-path
+	(append
+	 (mapcar (lambda (include-dirs) (concat "" include-dirs))
+		 (directory-files qt-include-path-prefix t "\\w+"))
+	 (split-string qt-include-path-prefix))))
 
 (provide 'flycheck-extend)
