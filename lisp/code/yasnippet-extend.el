7bc48ff2aaf59fce3a5944b58b513d44c96e3d38
Nopcall <nopcall[at]gmail.com>
2014-05-20 01:44:02 +0800

    fix some type mistake

--- lisp/code/yasnippet-extend.el
+++ lisp/code/yasnippet-extend.el
@@ -16,27 +16,26 @@
 			     yas-completing-prompt
 			     yas-ido-prompt))
 
-(setq yas-verbosity 1)								;; No need to be so verbose
-(setq yas-wrap-around-region t)							;; Wrap around region
+;; (setq yas-verbosity 1)								;; No need to be so verbose
+;; (setq yas-wrap-around-region t)							;; Wrap around region
 
 (defun yas-goto-end-of-active-field ()
   (interactive)
-  (let* ((snippet (car (yas-snippets-at-point)))
-	 (position (yas-field-end (yas-snippet-active-field snippet))))
+  (let* ((snippet (car (yas--snippets-at-point)))
+	 (position (yas--field-end (yas--snippet-active-field snippet))))
     (if (= (point) position)
-        (move-end-of-line)
-      (goto-char position))))							;; Inter-field navigation
-
+        (move-end-of-line 1)
+      (goto-char position))))
 (defun yas-goto-start-of-active-field ()
   (interactive)
-  (let* ((snippet (car (yas-snippets-at-point)))
-        (position (yas-field-start (yas-snippet-active-field snippet))))
+  (let* ((snippet (car (yas--snippets-at-point)))
+	 (position (yas--field-start (yas--snippet-active-field snippet))))
     (if (= (point) position)
-	(move-beginning-of-line)
+        (move-beginning-of-line 1)
       (goto-char position))))
-
 ;; (define-key yas-keymap (kbd "C-e") 'yas-goto-end-of-active-field)		;; Jump to end of snippet definition
 ;; (define-key yas-keymap (kbd "C-a") 'yas-goto-start-of-active-field)
+
 (define-key yas-keymap (kbd "M-<right>") 'yas-next-field-or-maybe-expand)
 (define-key yas-keymap (kbd "M-<left>") 'yas-prev-field)
 (define-key yas-keymap (kbd "<return>") 'yas-exit-all-snippets)
