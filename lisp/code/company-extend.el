ef10785c4889b4a5eaa88b99444ed5ebfa9968b3
Nopcall <nopcall[at]gmail.com>
2014-05-22 10:09:03 +0800

    support Qt completion

--- lisp/code/company-extend.el
+++ lisp/code/company-extend.el
@@ -15,6 +15,22 @@
 			     company-rtags company-gtags company-etags
 			     company-dabbrev-code company-keywords))))		;; company backends
 
+(defun qt-company ()
+  "Enable Qt library for buffer then make it local"
+  (interactive)
+  (when (not (boundp 'company-qt-include-path-prefix))
+    (setq company-qt-include-path-prefix "/home/Data/Qt5.3.0/5.3/gcc_64/include"))	;; set Qt library path unless it's been defined
+  (when (file-exists-p company-qt-include-path-prefix)
+    (setq company-clang-arguments
+	  (append
+	   (mapcar (lambda (include-dirs) (concat "-I" include-dirs))
+	   	   (directory-files company-qt-include-path-prefix t "\\w+"))
+	   (split-string (concat "-I" company-qt-include-path-prefix))
+	   company-clang-arguments)))
+  ;; (push "-code-completion-macros" company-clang-arguments)
+  ;; (push "-code-completion-patterns" company-clang-arguments)
+  (make-variable-buffer-local 'company-clang-arguments))
+
 (add-hook 'company-mode-hook
 	  (lambda ()
 	    (define-key company-active-map (kbd "<tab>") 'company-select-next)
